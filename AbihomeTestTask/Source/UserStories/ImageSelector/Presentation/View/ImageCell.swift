7863e3400f40177e1bc00d2396509be386317c85
Rafael Kayumov <justpoc@gmail.com>
2018-10-09 11:27:43 +0300

    Implement image selection

--- AbihomeTestTask/Source/UserStories/ImageSelector/Presentation/View/ImageCell.swift
+++ AbihomeTestTask/Source/UserStories/ImageSelector/Presentation/View/ImageCell.swift
@@ -12,11 +12,27 @@ class ImageCell: UICollectionViewCell {
 
     @IBOutlet private weak var imageView: UIImageView!
 
+    override func awakeFromNib() {
+        super.awakeFromNib()
+
+        contentView.layer.borderColor = UIColor.blue.cgColor
+    }
+
     var image: UIImage? {
         get {
             return imageView.image
         } set {
             imageView.image = newValue
         }
+    }
+
+    override var isSelected: Bool {
+        didSet {
+            setSelected(isSelected)
+        }
+    }
+
+    private func setSelected(_ selected: Bool) {
+        contentView.layer.borderWidth = selected ? 2 : 0
     }
 }
